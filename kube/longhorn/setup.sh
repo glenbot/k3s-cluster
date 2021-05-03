72889d38d3466ee53b3d056cffde734b6204042d
Glen Zangirolami <122543+glenbot@users.noreply.github.com>
2021-05-03 11:13:59 -0500

    Added longhorn ingress

--- kube/longhorn/setup.sh
+++ kube/longhorn/setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "Installing longhorn"
 
 if ! kubectl get ns "{{ (datasource "config").longhorn.namespace }}" &> /dev/null; then
@@ -7,5 +9,9 @@ fi
 
 # Install the helm chart
 helm upgrade -i longhorn longhorn/longhorn --version "{{ (datasource "config").longhorn.chart_version }}" -n "{{ (datasource "config").longhorn.namespace }}"
+
+if ! kubectl get ingressroute -n "{{ (datasource "config").longhorn.namespace }}" longhorn-frontend-external &> /dev/null; then
+  kubectl apply -n "{{ (datasource "config").longhorn.namespace }}" -f "${root_dir}/longhorn-frontend-ingress.yaml"
+fi
 
 echo "Done."
