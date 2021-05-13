d40a3d8f390a5f192e5b751652d437587365779d
Glen Zangirolami <122543+glenbot@users.noreply.github.com>
2021-05-13 10:13:58 -0500

    Added patch for argocd to allow for ingress to work

--- kube/argocd/setup.sh
+++ kube/argocd/setup.sh
@@ -5,7 +5,11 @@ if ! kubectl get ns "{{ (datasource "config").argocd.namespace }}" &> /dev/null;
     kubectl create ns "{{ (datasource "config").argocd.namespace }}"
 fi
 
+# Install argocd from url
 kubectl apply -n "{{ (datasource "config").argocd.namespace }}" -f "https://raw.githubusercontent.com/argoproj/argo-cd/{{ (datasource "config").argocd.version }}/manifests/install.yaml"
+
+# Patch API server deloyment for ingress
+kubectl patch deploy argocd-server -n argocd -p '{"spec": {"template": {"spec": {"containers": [{"name": "argocd-server", "command": ["argocd-server", "--insecure", "--staticassets", "/shared/app"]}]}}}}'
 
 if ! kubectl get ingressroute -n "{{ (datasource "config").argocd.namespace }}" argocd-frontend-external &> /dev/null; then
   kubectl apply -n "{{ (datasource "config").argocd.namespace }}" -f "${root_dir}/argocd-frontend-ingress.yaml"
